d659319ef7f056e42a0f26f330b00bbad9cf2efd
Alexandros Panagiotakis <dev@alpanagi.com>
2024-02-12 19:14:34 +0100

    Remove unnecessary indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -179,13 +179,13 @@ cmp.setup({
     })
 })
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  require('lspconfig')['rust_analyzer'].setup {
-    capabilities = capabilities
-  }
-  require('lspconfig')['tsserver'].setup {
-    capabilities = capabilities
-  }
-  require('lspconfig')['gdscript'].setup {
-    capabilities = capabilities
-  }
+require('lspconfig')['rust_analyzer'].setup {
+  capabilities = capabilities
+}
+require('lspconfig')['tsserver'].setup {
+  capabilities = capabilities
+}
+require('lspconfig')['gdscript'].setup {
+  capabilities = capabilities
+}
 
